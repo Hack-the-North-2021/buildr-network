2ef529093ef636330492f11b7594629c575d9a8e
Daniel Liu <mr.picklepinosaur@gmail.com>
2021-09-18 10:55:47 -0400

    reading and writing from socket

--- client/client.c
+++ client/client.c
@@ -1,10 +1,17 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
 
 #define SERVER_PORT 8080
 #define MAX_PACKET 4096
+
+typedef struct sockaddr Sockaddr;
+typedef struct sockaddr_in SockaddrIn;
 
 void error_die(const char* error_msg);
 
@@ -18,10 +25,10 @@ error_die(const char* error_msg)
 int
 main(int argc, char** argv)
 {
-    int socket_handle;
-    struct sockaddr_in addr;
+    int server_handle;
+    SockaddrIn addr;
 
-    if (socket_handle = socket(AF_INET, SOCK_STREAM, 0) < 0)
+    if ((server_handle = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         error_die("Cannot create the socket");
 
     bzero(&addr, sizeof(addr));
@@ -31,8 +38,25 @@ main(int argc, char** argv)
     if (inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr) <= 0)
         error_die("Invalid IP Address");
 
-    if (connect(socket_handle, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    if (connect(server_handle, (Sockaddr*)&addr, sizeof(addr)) < 0)
         error_die("Connection failed");
 
+    /* send message to server */
+    int send_len, n;
+    char send_data[MAX_PACKET];
+    char recv_data[MAX_PACKET];
+
+    snprintf(send_data, MAX_PACKET, "hello server");
+    send_len = strlen(send_data);
+    if (write(server_handle, send_data, send_len) != send_len)
+        error_die("Error writing to socket handle");
+
+    /* read response from server */
+    memset(recv_data, 0, MAX_PACKET);
+    while ((n = read(server_handle, recv_data, MAX_PACKET-1)) > 0) {
+        printf("%s", recv_data);
+        memset(recv_data, 0, MAX_PACKET);
+    }
+        
     return 0;
 }
--- server/server.c
+++ server/server.c
@@ -1,10 +1,17 @@
 #include <sys/socket.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
+#include <unistd.h>
 
 #define SERVER_PORT 8080
 #define MAX_PACKET 4096
+
+typedef struct sockaddr Sockaddr;
+typedef struct sockaddr_in SockaddrIn;
 
 void error_die(const char* error_msg);
 
@@ -19,7 +26,8 @@ int
 main(int argc, char** argv)
 {
     int client_handle;
-    struct sockaddr_in addr;
+    int connection_handle;
+    SockaddrIn addr;
 
     if ((client_handle = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         error_die("Cannot create the socket");
@@ -29,11 +37,44 @@ main(int argc, char** argv)
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_port = htons(SERVER_PORT);
 
-    if (bind(client_handle, (struct sockaddr_in*)&addr, sizeof(addr)) < 0)
+    if (bind(client_handle, (Sockaddr*)&addr, sizeof(addr)) < 0)
         error_die("Cannot bind");
 
     if (listen(client_handle, 10) < 0)
         error_die("Error listening");
+
+    while (1) {
+        SockaddrIn addr;
+        socklen_t addr_len;
+        char client_addr[MAX_PACKET];
+
+        printf("Listening for connections on port %d\n", SERVER_PORT);
+        connection_handle = accept(client_handle, (Sockaddr*)&addr, &addr_len);
+
+        inet_ntop(AF_INET, &addr, client_addr, MAX_PACKET-1);
+        printf("Client connected: %s\n", client_addr);
+
+        /* read from client */
+        int send_len, n;
+        char send_data[MAX_PACKET];
+        char recv_data[MAX_PACKET];
+
+        memset(recv_data, 0, MAX_PACKET);
+        n = read(client_handle, recv_data, MAX_PACKET-1);
+        while ((n = read(client_handle, recv_data, MAX_PACKET-1)) > 0) {
+            printf("%s\n", recv_data);
+            memset(recv_data, 0, MAX_PACKET);
+        }
+        if (n < 0) {
+            printf("%d\n", errno);
+            error_die("Error reading client message");
+        }
+        
+        snprintf(send_data, MAX_PACKET, "hello client");
+        send_len = strlen(send_data);
+        if (write(client_handle, send_data, send_len) != send_len)
+            error_die("Error writing to socket handle");
+    }
 
     return 0;
 }
